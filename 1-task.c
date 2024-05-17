860b9ef8d12538e99c4c7ffec3a99cf1a6f44ee2
Kyrylo Shykhalov <kshykhalov@kse.org.ua>
2024-05-17 12:33:55 +0300

    Update 1-task.c
    upgrade conditionals

--- 1-task.c
+++ 1-task.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#define BufferSize 1024
+#define start true
 
-//Function Declarations
+char text[100][BufferSize];
+int totalLines = 0;
+
+//Function Declarations need to declarate and then in any part of code write it
 void appendText(char *text);
 void newLine();
 void saveToFile(const char *filename);
@@ -9,22 +16,28 @@ void loadFromFile(const char *filename);
 void printText();
 void insertText(int line, int index, char *str);
 int searchText(char *str);
+void clearConsole() ;
 
 int main(void){
-    int choice;
-    //char inputBuffer[BUFFER_SIZE];
+    int user_input;
+    char inputBuffer[BufferSize];
     int line, index;
     char filename[100];
 
-    while (true){
-        int user_input;
+    while (start){
         printf("\nChoose the command:\n ");
         scanf("%d",  user_input);
         if (user_input == 1){
             printf("%s","1. Enter text to append\n");
+            fgets(inputBuffer, BufferSize, stdin);
+            inputBuffer[strcspn(inputBuffer, "\n")] = 0; // Remove newline character
+            appendText(inputBuffer);
+            break;
         }
         else if (user_input == 2){
             printf("%s","2. Start new line\n");
+            newLine();
+            break;
         }
         else if (user_input == 3){
             printf("%s","3. Enter the file name for saving:\n ");
@@ -44,14 +57,34 @@ int main(void){
             printf("%s","6. Choose line and index:\n ");
             scanf("%d %d", &line, &index);
             getchar();
+            printf("Enter text to insert: ");
+            fgets(inputBuffer, BufferSize, stdin);
+            inputBuffer[strcspn(inputBuffer, "\n")] = 0; // Remove newline character
+            insertText(line, index, inputBuffer);
         }
         else if (user_input == 7){
             printf("%s","7. Search text position");
+            fgets(inputBuffer, BufferSize, stdin);
+            inputBuffer[strcspn(inputBuffer, "\n")] = 0; // Remove newline character
+            if (searchText(inputBuffer) == 0) {
+                printf("Text not found.\n");
+            } else {
+                printf("Text found at position %d.\n", searchText(inputBuffer));
+            }
         }
-        else{
-            printf("%s", "Invalid choice. Please try again.\n");
+        else if (user_input == 8) {
+            printf("%s","8. Clear the console");
+            clearConsole(); // Clear the console
+        } else if (user_input == 9) {
+            return 0;
         }
-
     }
     return 0;
+}
+void clearConsole() {
+#ifdef _WIN64
+    system("cls");
+#else
+    system("clear");
+#endif
 }
